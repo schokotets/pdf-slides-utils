9d8fbaeddf0f3ffefcbb075e2c865023b41d7f22
Hannah Markfort <74815681+xCatalitY@users.noreply.github.com>
2024-04-19 06:29:03 +0000

    Removed .idea files, added default shell expansion

--- pdfdeanimate-image.py
+++ pdfdeanimate-image.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env nix-shell
+#!/usr/bin/env python3
 #!nix-shell -i python3 -p poppler_utils pdftk python38 python38Packages.numpy python38Packages.pillow
 
 import subprocess
@@ -6,10 +6,10 @@ import os
 import glob
 import numpy
 from PIL import Image
+import sys
 
 def process_pdf(pdffile):
     pdffile_name = pdffile.rsplit(".", 1)[0]
-
     pgmdir_name = pdffile_name + "-pgm"
     pgmfile_name = pgmdir_name + "/" + pdffile_name
 
@@ -22,14 +22,12 @@ def process_pdf(pdffile):
 
     lastpix = None
     haslastpix = False
-
     containpages = []
     currenthold = -1
 
     filelist = glob.glob(os.path.join(pgmdir_name, '*.pgm'))
     for filename in sorted(filelist, key=lambda s: s.lower()):
         pagenr = filename.rsplit("/", 1)[-1].rsplit(".", 1)[0].rsplit("-", 1)[-1]
-
         img = Image.open(filename)
         pix = numpy.array(img)
         img.close()
@@ -44,11 +42,15 @@ def process_pdf(pdffile):
         currenthold = pagenr
 
     containpages.append(currenthold)
-
     print(f"reduced {len(filelist)}-page PDF to {len(containpages)} pages")
-
     subprocess.run(["pdftk", pdffile, "cat"] + containpages + ["output", "stripped-" + pdffile])
 
-# Iterate through all PDF files in the current directory
-for pdffile in glob.glob('*.pdf'):
-    process_pdf(pdffile)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        # Process PDF files specified as command line arguments
+        for pdffile in sys.argv[1:]:
+            process_pdf(pdffile)
+    else:
+        # Process all PDF files in the current directory
+        for pdffile in glob.glob('*.pdf'):
+            process_pdf(pdffile)
